eda2c844269a50742c1ba413e670586602294e93
Mayank Singh Shakya <mayank1807.singh@gmail.com>
2023-02-11 13:45:11 +0530

    first commit

---
+++ ArrayProblems/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<< "Mr Singh is Here!  \n";
+    
+
+    int i = 5, j = 9;
+    cout<< i+j << " \n";
+    return 0;
+}
